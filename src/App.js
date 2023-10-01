c5c91ffa8564aa5f382f4349567a976ef1fd64a0
Piotr Krupa <piotrkrupa.email@gmail.com>
2023-10-01 21:46:59 +0200

    stoper app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Container from './Components/Container/Container';
+import Timer from './Components/Timer/Timer';
+
+
+const App = () => {  
+  return (
+
+      <Container>
+      <Timer />
+      </Container>
+  );
+}
+
+export default App;
---
+++ src/Components/Timer/Timer.js
@@ -0,0 +1,45 @@
+import styles from './Timer.module.scss'
+import Button from '../Button/Button'
+import FormattedTime from '../FormattedTime/FormattedTime';
+import { useState, useEffect } from 'react';
+
+
+const Timer = () => {
+    const [time, setTime] = useState(0)
+    const [timer, setTimer] = useState('')
+
+    const start = () => {
+        setTimer(
+          setInterval(() => {
+            setTime((prevValue) => prevValue + 1);
+          }, 1)
+        );
+      };
+    
+      useEffect(() => {
+        return () => {
+          if (timer) clearInterval(timer);
+        };
+      }, [timer]);
+    
+      const handleStart = () => {
+        start();
+      };
+      const handleStop = () => {
+        clearInterval(timer);
+      };
+      const handleReset = () => {
+        setTime(0);
+      };
+
+    return (
+    <div>
+        <FormattedTime time={time} />
+        <Button action={handleStart}>Start</Button>
+        <Button action={handleStop}>Stop</Button>
+        <Button action={handleReset}>Reset</Button>
+    </div>
+    )
+}
+
+export default Timer; 
